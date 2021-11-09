fc8b2af27d85602b7da601f318ccaf43c4f907af
ekta chandani <ekta.chandani@tothenew.com>
2021-11-09 23:26:29 +0530

    splash screen

--- app/src/main/java/com/example/navigationjetpack/fragment/FragmentLoginFragment.kt
+++ app/src/main/java/com/example/navigationjetpack/fragment/FragmentLoginFragment.kt
@@ -1,15 +1,22 @@
 package com.example.navigationjetpack.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.navigationjetpack.MainActivity
 import com.example.navigationjetpack.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class FragmentLoginFragment : Fragment() {
 
+    lateinit var sharedPreferences:SharedPreferences
+    var isRemembered = false
 
 
     override fun onCreateView(
@@ -18,6 +25,19 @@ class FragmentLoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+        sharedPreferences = this.requireContext().getSharedPreferences("Pref" , Context.MODE_PRIVATE)
+        isRemembered = sharedPreferences.getBoolean("CHECKBOX" , false)
+
+//        if (isRemembered){
+//            val intent = Intent(this.context , FragmentHomeFragment::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+//        bt_login.setOnClickListener {
+//
+//        }
+
     }
 
 }
